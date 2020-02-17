c3182eb410cb88a1fcd989024f269f8c55534c51
Gerard Capes <gerard.capes@manchester.ac.uk>
2020-02-17 10:07:21 +0000

    Move chemical potential module to separate file

--- optimiser/opt_vle.f90
+++ optimiser/opt_vle.f90
@@ -1,165 +1,4 @@
-!***************************************************************************************************
-module Mu_mod
-!***************************************************************************************************
-!Modules
-!=======    
-    use Types           ! Definitions of types and double precision
-    use Global          ! Important global parameters
-    use Setup           ! To setup the system
-    use Ideal           ! Calculate ideal A
-    use Mono            ! Calculate mono A
-    use Chain           ! Calculate chain A
-    use Assoc           ! Calculate assoc A
-    use Ion             ! Calculate ion A
-    use Diff            ! Differentials module  
-!***************************************************************************************************
-    implicit none
-!***************************************************************************************************
-    contains
-!***************************************************************************************************    
-    function Mu( i_comp ) result(mu_out)
-        implicit none
-        
-        integer, intent(in)      ::  i_comp
-        
-        real(kind=DP)            ::  mu_out
-        real(kind=DP)            ::  mu_ideal, mu_mono, mu_chain, &
-                                    &   mu_ion, mu_born, mu_assoc   
-       
-        !ensure nsum is set
-        Comp_array(:)%nm = Comp_array(:)%xi
-        Comp_array(:)%nm = Comp_array(:)%nm * NA
-        nsum = sum(Comp_array(:)%nm)
 
-        !call differential set up
-        call Ndiffs( i_comp ) 
-        
-        mu_ideal = A_ideal_dn( i_comp ) * KB * t * NA
-        mu_mono  = A_mono_dn( ) * NA * KB * t       
-        mu_assoc = A_assoc_dn( ) * NA * KB * t
-        mu_born  = A_born_dn( i_comp ) * KB * t * nsum * NA
-        mu_ion   = A_ion_dn( i_comp ) * NA * KB * t
-        mu_chain = A_chain_dn( i_comp ) * NA
-
-        mu_out = mu_ideal + mu_mono + mu_assoc + mu_chain + mu_born + mu_ion
-!        print*,i_comp,mu_out,mu_ideal,mu_mono,mu_chain,mu_assoc,mu_born,mu_ion
-        
-        return
-    end function Mu
-!***************************************************************************************************
-    function Mu_neut( i_comp ) result(mu_out)
-        implicit none
-        
-        integer, intent(in)      ::  i_comp
-        
-        real(kind=DP)            ::  mu_out
-        real(kind=DP)            ::  mu_ideal, mu_mono, mu_chain, mu_assoc   
-       
-        !ensure nsum is set
-        Comp_array(:)%nm = Comp_array(:)%xi
-        Comp_array(:)%nm = Comp_array(:)%nm * NA
-        nsum = sum(Comp_array(:)%nm)
-
-        !call differential set up
-        call Ndiffs( i_comp ) 
-        
-        mu_ideal = A_ideal_dn( i_comp ) * KB * t * NA
-        mu_mono  = A_mono_dn( ) * NA * KB * t       
-        mu_assoc = A_assoc_dn( ) * NA * KB * t
-        mu_chain = A_chain_dn( i_comp ) * NA
-
-        mu_out = mu_ideal + mu_mono + mu_assoc + mu_chain 
-!        print*,i_comp,mu_out,mu_ideal,mu_mono,mu_chain,mu_assoc
-        
-        return
-    end function Mu_neut
-!***************************************************************************************************
-    function Mu_id( i_comp ) result(mu_out)
-        implicit none
-        
-        integer, intent(in)      ::  i_comp
-        
-        real(kind=DP)            ::  mu_out
-        real(kind=DP)            ::  mu_ideal, mu_chain
-
-       
-        !ensure nsum is set
-        Comp_array(:)%nm = Comp_array(:)%xi
-        Comp_array(:)%nm = Comp_array(:)%nm * NA
-        nsum = sum(Comp_array(:)%nm)
-
-        !call differential set up
-        call Ndiffs( i_comp ) 
-        
-        mu_ideal = A_ideal_dn( i_comp ) * KB * t * NA
-        mu_chain = A_chain_dn( i_comp ) * NA
-
-        mu_out = mu_ideal + mu_chain 
-        !print*,i_comp,mu_out,mu_ideal,mu_mono,mu_chain,mu_born,mu_ion,mu_assoc
-        
-        return
-    end function Mu_id
-!***************************************************************************************************
-    function Mu_res( i_comp ) result(mu_out) !As above, sans ideal
-        implicit none
-        
-        integer, intent(in)      ::  i_comp
-        
-        real(kind=DP)            ::  mu_out
-        real(kind=DP)            ::  mu_mono, mu_chain, &
-                                    &   mu_ion, mu_born, mu_assoc   
-    
-        !ensure nsum is set
-        Comp_array(:)%nm = Comp_array(:)%xi
-        Comp_array(:)%nm = Comp_array(:)%nm * NA
-        nsum = sum(Comp_array(:)%nm)
-
-        !call differential set up
-        call Ndiffs( i_comp ) 
-        
-        mu_mono  = A_mono_dn( ) * NA * KB * t 
-        mu_assoc = A_assoc_dn( ) * NA * KB * t
-        mu_born  = A_born_dn( i_comp ) * KB * t * nsum * NA
-        mu_ion   = A_ion_dn( i_comp ) * NA * KB * t
-        mu_chain = A_chain_dn( i_comp ) * NA
-
-        mu_out = mu_mono + mu_assoc + mu_chain + mu_born + mu_ion
-!        print*,i_comp,mu_out,mu_mono,mu_chain,mu_born,mu_ion,mu_assoc
-        
-        return
-    end function Mu_res
-!***************************************************************************************************  
-    function Mu_res_inf( i_comp ) result(mu_out) !As above, sans ideal
-        implicit none
-        
-        integer, intent(in)      ::  i_comp
-        
-        real(kind=DP)            ::  mu_out
-        real(kind=DP)            ::  mu_mono, mu_chain, &
-                                    &   mu_born, mu_assoc   
-                
-        !ensure nsum is set
-        Comp_array(:)%nm = Comp_array(:)%xi
-        Comp_array(:)%nm = Comp_array(:)%nm * NA
-        nsum = sum(Comp_array(:)%nm)
-
-        !call differential set up
-        call Ndiffs( i_comp ) 
-        
-        mu_mono  = A_mono_dn( ) * NA * KB * t 
-        mu_assoc = A_assoc_dn( ) * NA * KB * t
-        mu_born  = A_born_dn( i_comp ) * KB * t * nsum * NA
-        if(mu_born/=mu_born) mu_born=0.0e0_DP        
-        mu_chain = A_chain_dn( i_comp ) * NA
-
-        mu_out = mu_mono + mu_assoc + mu_chain + mu_born
-
-        return
-    end function Mu_res_inf
-!***************************************************************************************************
-
-!***************************************************************************************************
-end module Mu_mod
 !***************************************************************************************************
 !***************************************************************************************************
 !   SAFT Module to:
@@ -174,13 +13,13 @@ module Vol_mod
     use Types           ! Definitions of types and double precision
     use Global          ! Important global parameters
     use Setup           ! To setup the system
-    use Pressure           ! Calculate pressure
+    use Pressure        ! Calculate pressure
     use Ideal           ! Calculate ideal A
     use Mono            ! Calculate mono A
     use Chain           ! Calculate chain A
     use Assoc           ! Calculate assoc A
     use Ion             ! Calculate ion A
-    use Mu_mod              ! Calculate chem pot
+    use ChemPot         ! Calculate chem pot
 !***************************************************************************************************
     implicit none
 !***************************************************************************************************
@@ -797,7 +636,7 @@ end module Quote_mod
 	  use Types           ! Definitions of types and double precision
 	  use Global          ! Important global parameters
 	  use Pressure
-	  use Mu_mod
+	  use ChemPot
     use Vol_mod
         !     .. Implicit None Statement ..
       Implicit None
@@ -917,7 +756,7 @@ End
 	  use Input_opt   ! Read optimisation parameters
 	  use Vol_mod
 	  use c05qbfe_mod
-	  use Mu_mod
+	  use ChemPot
 !     .. Implicit None Statement ..
       Implicit None
 !     .. Accessibility Statements ..
